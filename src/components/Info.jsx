8a9b53ccf0f5905ce1287496d601e5d6f0d6ca0f
Vincent Le <vincentle.fh@gmail.com>
2022-10-12 00:29:14 +0300

    Updated info

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -5,7 +5,8 @@ const Info = () => {
     <div className="content">
       <Title>Info</Title>
       <ul>
-        <li>The cryptosystem encrypts and decrypts plaintext Symmetric algorithms</li>
+        <li>The cryptosystem encrypts and decrypts plaintext using Symmetric algorithms</li>
+        <li>The app doesn't save messages  from the users to any database for safety purpose</li>
         <li>Plaintext must only consists of alphanumeric characters, blank space and these following symbols ,.-:'?</li>
         <li>Key will be auto generated for you</li>
       </ul>
